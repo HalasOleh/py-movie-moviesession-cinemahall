a4ab5c7db67fda607e57a120ab899dd047b52b02
Oleh Halas <oleggalas8@gmail.com>
2026-05-06 13:42:28 +0300

    Implement MovieCinemaHall database

--- db/models.py
+++ db/models.py
@@ -14,3 +14,40 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+    @property
+    def capacity(self) -> object:
+        return self.rows * self.seats_in_row
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(CinemaHall,
+                                    related_name="sessions",
+                                    on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,
+                              related_name="sessions",
+                              on_delete=models.CASCADE)
+    test = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{self.movie} {self.show_time}"
